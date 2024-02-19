6161d4915bd8ff94cd9c5744f3baa1b21abbeccf
wkipngetich <wkipngetich@co-opbank.co.ke>
2024-02-19 17:29:18 +0300

    updates on nest js api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+import { Body } from "@nestjs/common";
 
 @Controller("auth")
 export class AuthController {
@@ -12,7 +14,8 @@ export class AuthController {
   }
 
   @Post("signup")
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({dto})
     return this.authService.signup()
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,59 @@
 import { Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import * as argon2 from "argon2";
+
+
+import { ForbiddenException } from "@nestjs/common";
 
 @Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService) { }
 
-    //constructor(private authService: AuthService) { }
 
 
-    login() {
-        return {
-            msg: "Lets login in"
+    login(authDto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: authDto.email
+            }
+        });
+        if (!user) {
+            throw new ForbiddenException("Invalid user!");
+
         }
 
+        const passwordMatches = await argon2.verify(user.hash, authDto.password);
+        if (!passwordMatches) {
+            throw new ForbiddenException("Incorrect Credentials!");
+
+        }
+        return user;
     }
 
 
-    signup() {
-        return {
-            msg: "Lets sign up "
+    async signup(authDto: AuthDto) {
+        try {
+
+            //generate password
+            //save user
+            // const argon2 = require('argon2');
+
+            const hash = await argon2.hash(authDto.password);
+            const user = await this.prisma.user.create({
+                data: {
+                    email: authDto.email,
+                    hash
+                },
+            });
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException("Credentials Taken",)
+                } throw error;
+
+            }
         }
     }
 }
