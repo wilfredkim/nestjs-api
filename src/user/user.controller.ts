cc5909a62e8ed756cb1bba785187ffaea9a73f32
Wilfred <wilfredkim5@gmail.com>
2024-03-03 17:11:59 +0300

    update on nest js

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) { }
 
     @Get('me')
     //@UseGuards(JwtGuard)
     getMe(@GetUser() user: User) {
         return user;
     }
+
+    @Patch()
+    editUser(
+        @GetUser('id') userId: number,
+        @Body() dto: EditUserDto,
+    ) {
+        return this.userService.editUser(userId, dto);
+    }
+
+
+
+
 
 }
